986de1ad927c15a5bb28dd1e7daa80c55ca2e535
katyasharko27 <ks@cloudprinter.com>
2022-09-01 16:13:42 +0300

    some additional changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import './index.css';
 import 'tachyons';
+import { createLogger} from "redux-logger/src";
 import App from './containers/App';
+import { searchRobots, requestRobots } from "./reducers";
+import thunkMiddleware from 'redux-thunk';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
+const logger = createLogger();
+
+const rootReducer = combineReducers({searchRobots, requestRobots});
+
+const Store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Provider store={Store}><App /></Provider>);
 
