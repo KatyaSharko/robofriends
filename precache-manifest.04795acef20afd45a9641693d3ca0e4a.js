a2a71ba6ba82161323568e6a507849f19bd3c2d3
katyasharko27 <ks@cloudprinter.com>
2022-09-01 16:36:45 +0300

    Updates

---
+++ precache-manifest.04795acef20afd45a9641693d3ca0e4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "889ef27063055d9609f1",
+    "url": "/robofriends/static/css/main.69b209a7.chunk.css"
+  },
+  {
+    "revision": "889ef27063055d9609f1",
+    "url": "/robofriends/static/js/main.c784d8bb.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "0be1a09bcc77f5455df3",
+    "url": "/robofriends/static/css/2.5ae8720e.chunk.css"
+  },
+  {
+    "revision": "0be1a09bcc77f5455df3",
+    "url": "/robofriends/static/js/2.33310d93.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "5909d9d27fa549a5d359fdc7a45aa273",
+    "url": "/robofriends/index.html"
+  }
+];
